991f4407a3ce0de37d7b154d2c06cbae81c1e8f5
bhikandeshmukh <thebhikandeshmukh@gmail.com>
2025-12-16 12:07:13 +0530

    Fix duplicate ComponentActivity import in MainActivity.kt

--- android-app/app/src/main/java/com/attendance/tracker/MainActivity.kt
+++ android-app/app/src/main/java/com/attendance/tracker/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.activity.ComponentActivity
 import com.attendance.tracker.ads.AdMobManager
 import com.attendance.tracker.ads.BannerAdView
 import com.attendance.tracker.data.model.User
